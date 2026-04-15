5a0413d2668cff310f22f756562208145ab31171
sireesha1922 <Vaishusiriv@gmail.com>
2026-04-15 15:27:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def register():
 
         return redirect("/login")
 
-    return render_template("register.html")
+   return render_template("dashboard.html", ...)
 
 # ---------------- LOGIN ----------------
 @app.route("/login", methods=["GET", "POST"])
